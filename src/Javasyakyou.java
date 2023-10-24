d30dd59847fc83e2b63aed73245c1a09647888ce
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-10-24 22:04:59 +0900

    Java2-7

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,9 +2,7 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		System.out.println("例外が");
-		System.out.println("発生しました。");
-
+		System.out.println("例外が\r\n発生しました。");
 
 
 	}
