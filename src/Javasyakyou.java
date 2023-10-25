388b7635364c8bee3c6fb56f956791e917189454
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-10-25 20:02:25 +0900

    Java3-2

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,11 +2,8 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		int suugaku=45;
-		int eigo=82;
-		if(suugaku>50||eigo>50) {
-			System.out.println("この人は合格です。");
-		}
+		String name="高橋";
+		System.out.println(name);
 
 	}
 
