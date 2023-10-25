11fac2f461c7f6e23ac71274c156145f997a4cb4
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-10-25 21:36:53 +0900

    Java4-27

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -3,10 +3,7 @@ public class Javasyakyou {
 
 	public static void main(String[] args) {
 		int a=10;
-		System.out.println(a++);
-		System.out.println(a++);
-		System.out.println(a++);
-		System.out.println(a++);
+		System.out.println(--a);
 
 	}
 
