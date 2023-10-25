1c15fdccb023271580ed846ad377a54ea732a817
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-10-25 20:05:00 +0900

    Java3-4

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,8 +2,8 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		String str1="鈴木";
-		System.out.println(str1);
+		String str2="Tanaka";
+		System.out.println(str2);
 
 	}
 
