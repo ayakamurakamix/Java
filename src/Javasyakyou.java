ee8938286ffc06847e6ac4a2b5bc8d888d129191
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-06 21:26:17 +0900

    Java6-6

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,15 +2,18 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		int a=2;
-		if(a>0)
-			if(a==2) {
-				System.out.println("A");
-			}else if(a==3) {
-				System.out.println("B");
+		int number=88;
+		if(number<=100) {
+			if(number<20) {
+				System.out.println("未成年");
+			}else if(number==77) {
+				System.out.println("喜寿");
+			}else if(number==88) {
+				System.out.println("米寿");
 			}else {
-				System.out.println("C");
+				System.out.println("成人");
 			}
+		}
 
 	}
 
