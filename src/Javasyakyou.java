d6ebc6e7c7955bbcf317383acae56c0aebb1c5a4
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-06 21:38:19 +0900

    Java8-8

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,8 +2,9 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		int[]i= {1,5,10};
-		System.out.println(i[1]);
+		String[]name= {"田中","高橋","斉藤"};
+		name[0]="加藤";
+		System.out.println(name[0]);
 
 	}
 
