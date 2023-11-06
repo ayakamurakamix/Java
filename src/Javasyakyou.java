95f6dba7e632abda29dd9ed6a0a9d61e8cb3442f
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-06 21:32:50 +0900

    Java8-1

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,17 +2,11 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		String a="青";
-		switch(a) {
-		case "赤":
-			System.out.println("赤組です");
-			break;
-		case "白":
-			System.out.println("白組です");
-			break;
-		default:
-			System.out.println("エラーです");
-		}
+		String[]name=new String[3];
+		name[0]="田中";
+		name[1]="高橋";
+		name[2]="斉藤";
+		System.out.println(name[0]);
 
 	}
 
