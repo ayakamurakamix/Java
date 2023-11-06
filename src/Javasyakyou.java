1dd81cad7adedcbab5a2f74deb4e10975cf5f996
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-06 21:23:08 +0900

    Java6-2

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,9 +2,9 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		int a=20;
-		if(a>10) {
-			if(a<30) {
+		int a=7;
+		if(a>3) {
+			if(a<10) {
 				System.out.println("A");
 			}
 		}
