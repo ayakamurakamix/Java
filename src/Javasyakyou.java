3dfef1f908873098c1494abba0e42305bc2b6405
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-07 20:10:57 +0900

    Java10-11

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,8 +2,8 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		for(int i=10;i>1;i-=3) {
-			System.out.println(i);
+		for(int i=1;i<=5;i++) {
+			System.out.println("★");
 		}
 
 	}
