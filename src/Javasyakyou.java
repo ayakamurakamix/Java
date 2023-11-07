0e9b7dec96dbea5f4b96dc55ed1cf9c95c614b85
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-07 20:48:48 +0900

    Java13-5

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,11 +2,11 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		int i=5;
+		int i=1;
 		do {
 			System.out.println(i);
-			i--;
-		}while(i>0);
+			i+=2;
+		}while(i<=10);
 
 	}
 
