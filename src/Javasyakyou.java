23804feabbd9fc83a7f9cdf0adcba1d404888cf0
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-07 19:57:51 +0900

    Java10-1

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,14 +2,9 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		String[][]country=new String[3][2];
-		country[0][0]="日本";
-		country[0][1]="タイ";
-		country[1][0]="アメリカ";
-		country[1][1]="ブラジル";
-		country[2][0]="フランス";
-		country[2][1]="ロシア";
-		System.out.println(country[1][1]);
+		for(int i=0;i<3;i++) {
+			System.out.println(i);
+		}
 
 	}
 
