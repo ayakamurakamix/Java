e1aef5b5a5daadb63718162ddbd3e9421edb655c
ayakamurakamix <ayaka.murakami1204@gmail.com>
2023-11-07 20:13:24 +0900

    Java11-3

--- src/Javasyakyou.java
+++ src/Javasyakyou.java
@@ -2,11 +2,9 @@
 public class Javasyakyou {
 
 	public static void main(String[] args) {
-		for(int a=0;a<3;a++) {
-			for(int b=1;b<4;b++) {
-				System.out.println(b);
-			}
-		}
+		int a=10;
+		String b=a>0?"プラス":"マイナス";
+		System.out.println(b);
 
 	}
 
